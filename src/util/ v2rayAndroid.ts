23ee7d78c111c9ca359a8142e19a36e7b584b82d
pengchujin <pengchujin@blackbeanssoft.com>
2018-12-12 00:44:22 +0800

    fix android v2ray

--- src/util/ v2rayAndroid.ts
+++ src/util/ v2rayAndroid.ts
@@ -2,6 +2,9 @@ export function v2rayAndroid(nodes) {
   let v2rayServers = ''
   for(let node of nodes) {
     if(node.type == 'V2RAY') {
+      node.info.v = "2"
+      node.info.path = node.info.path.replace(/\//, '')
+      delete node.info.method
       let baseV2ray =  Buffer.from(JSON.stringify(node.info)).toString('base64')
       let server = Buffer.from('vmess://' + baseV2ray)
       v2rayServers = v2rayServers + server + '\n'
